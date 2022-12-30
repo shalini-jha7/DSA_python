690fdef980a64d4d1442e109efeec8f06da7e1a3
shalini <121378335+shalini-jha7@users.noreply.github.com>
2022-12-30 18:58:17 +0530

    Update power.py

--- Recursion/power.py
+++ Recursion/power.py
@@ -1,9 +1,11 @@
-def power(base, exp)
-  if base == 0:
-  assert int(exp) == exp
-    return 0
-   elif exp<0:
+def power(base, exp):
+  if exp == 0:
+    return 1
+  elif exp<0:
     return 1/base * power(base,exp+1)
+  elif exp ==1:
+    return(base)
   return base * power(base,exp-1)
-
+  
+print(power(8,-1))
   
