2bf0e0eb55ebd6cdf0bcd1c6f0e3d96048934421
shalini <121378335+shalini-jha7@users.noreply.github.com>
2022-12-31 11:04:34 +0530

    Create createtuple.py

---
+++ Tuple/createtuple.py
@@ -0,0 +1,9 @@
+# tuple is immutable sequence of python objects
+# creating a tuple: it is done by using keyword tuple.
+newTuple = tuple('abc')
+print(newTuple)
+newTuple = tuple('123')
+print(newTuple)
+
+#time ccomplexity for creating tuple is O(1)
+# space complexity for creating a tuple is O(n), as it depends on the number of elemets in it
