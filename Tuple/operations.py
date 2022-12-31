d21d79a6c80711134ddc6290199a410b39475f72
shalini <121378335+shalini-jha7@users.noreply.github.com>
2022-12-31 12:56:51 +0530

    Create operations.py

---
+++ Tuple/operations.py
@@ -0,0 +1,13 @@
+# Tuple Operations / Functions
+myTuple = (1,4,3,2,5)
+myTuple1 = (1,2,6,9,8,7)
+mytup =1,2  # this is also a tuple , lets find type usinh operator
+
+print(type(mytup))
+
+print(myTuple + myTuple1) 
+print(myTuple * 4)      
+print(2 in myTuple1)
+
+myTuple1.count(2)
+myTuple1.index(2)
