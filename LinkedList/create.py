761c62ff6d3bddd8d42ef7b8c609cf590c6f819a
shalini <121378335+shalini-jha7@users.noreply.github.com>
2023-01-02 20:09:18 +0530

    Create create.py

---
+++ LinkedList/create.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
