2ca6f08d4b707016cce53abbc7dcc38ce5622859
shalini <121378335+shalini-jha7@users.noreply.github.com>
2023-01-02 16:28:58 +0530

    Update checkduplicate.py

--- List/problems/checkduplicate.py
+++ List/problems/checkduplicate.py
@@ -1,9 +1,30 @@
 # check duplicate in a list
 
+# method 1: by using 2 for loops
+
 def check(arr):
-  for i,j in range(len(arr)):
-    if arr[i] ==arr[j]:
-      return True
-    return False
-  arr =[1,2,3,1]
-  print(check)
+    for i in range(len(arr)):
+        for j in range(i+1,len(arr)):
+            if(arr[i] ==arr[j]):
+                
+                print("yes duplicate")
+                break
+    print("no duplicate")
+nums =[1,2,3,4,5,6,6]
+check(nums)
+
+#method 2, 
+def check(arr):
+    a=[]
+    for i in arr:
+        if i in a :
+            print("duplicate for ",i,"is there")
+            return False
+            
+        else:
+            a.append(i)
+    return True
+      
+nums =[1,2,3,4,5,6,6,7,8,2]
+
+print(check(nums))
