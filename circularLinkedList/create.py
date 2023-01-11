4291d9902ff429918eb569b841393811414ae94d
shalini <121378335+shalini-jha7@users.noreply.github.com>
2023-01-11 21:26:31 +0530

    Update create.py

--- circularLinkedList/create.py
+++ circularLinkedList/create.py
@@ -1,1 +1,26 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
 
+class CircularSinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+            if node == self.tail.next:
+                break
+            
+
+    #  Creation of circular singly linked list
+    def createCSLL(self, nodeValue):
+        node = Node(nodeValue)
+        node.next = node
+        self.head = node
+        self.tail = node
+        return "The CSLL has been created"
