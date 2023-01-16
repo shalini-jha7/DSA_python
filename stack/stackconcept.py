bf815010afe42be01ef85755dd0d24e6ab16163f
shalini <121378335+shalini-jha7@users.noreply.github.com>
2023-01-16 11:48:55 +0530

    Create stackconcept.py

---
+++ stack/stackconcept.py
@@ -0,0 +1,17 @@
+#createstack
+ customStack = []      # now its empty
+ . # create a stack using list first
+class stack:
+  def __init__(self):
+    self.list = []
+  def __str__(self):
+    values = self.list.reverse()
+    values = [str(x) for x in self.list]
+   
+#push operation 
+customStack.push()    
+# pop 
+# peek operation. , return the top element
+#isEmpty() method
+#isFull() operation
+#delete()   , for deletion of entire stack  
